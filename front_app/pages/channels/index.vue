65e410f33dd063d4c701d0b228db227d3c1f1283
haiguanghu <huhaiguang@me.com>
2022-02-14 21:05:56 +0900

    test v-calendar

--- front_app/pages/channels/index.vue
+++ front_app/pages/channels/index.vue
@@ -1,6 +1,16 @@
 <template>
 <div>
     <Header />
+    <Calendar :attributes="attributes">
+        <div slot="day-popover" slot-scope="{ day, dayTitle, attributes }">
+            <div class="text-xs text-gray-300 font-semibold text-center">
+                {{ dayTitle }}
+            </div>
+            <popover-row v-for="attr in attributes" :key="attr.key" :attribute="attr">
+                {{ attr.customData.description }}
+            </popover-row>
+        </div>
+    </Calendar>
     <Slider />
     <About />
     <!-- <ProductSlider :products="products" :category="category" @openQuickview="showQuickview" @openCompare="showCoampre" @openCart="showCart" /> -->
@@ -14,6 +24,9 @@
 </template>
 
 <script>
+import Calendar from 'v-calendar/lib/components/calendar.umd'
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+import PopoverRow from 'v-calendar/lib/components/popover-row.umd.min'
 import {
     mapState
 } from 'vuex'
@@ -38,11 +51,27 @@ export default {
         Blog,
         // Instagram,
         // cartRightModel,
+        Calendar,
+        DatePicker,
+        PopoverRow
     },
     data() {
         return {
             products: [],
             category: [],
+            calendar_attrs: [{
+                key: 'today',
+                highlight: true,
+                dates: new Date(),
+            }, ],
+            todos: [{
+                description: 'Take Noah to basketball practice.',
+                isComplete: false,
+                dates: {
+                    weekdays: 6
+                }, // Every Friday
+                color: 'red',
+            }, ]
             // showquickviewmodel: false,
             // showcomparemodal: false,
             // showcartmodal: false,
@@ -51,9 +80,22 @@ export default {
         }
     },
     computed: {
-        ...mapState({
-            // productslist: state => state.products.productslist
-        })
+        attributes() {
+            return [
+                // Attributes for todos
+                ...this.todos.map(todo => ({
+                    dates: todo.dates,
+                    dot: {
+                        color: todo.color,
+                        class: todo.isComplete ? 'opacity-75' : '',
+                    },
+                    popover: {
+                        label: todo.description,
+                    },
+                    customData: todo,
+                })),
+            ];
+        }
     },
     mounted() {
         // this.productsArray()
